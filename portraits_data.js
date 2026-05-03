d4942927f2775bcc3078352fbbf677d583e05b16
jadeliu80 <2101294532@qq.com>
2026-05-03 23:39:32 +0200

    Delete portraits_data.js

--- portraits_data.js
+++
@@ -1,7 +0,0 @@
-const PORTRAITS = {
-  nezha: 'data:image/png;base64,'$(base64 -w0 portraits/nezha.png)',
-  aobing: 'data:image/png;base64,'$(base64 -w0 portraits/aobing.png)',
-  leotong: 'data:image/png;base64,'$(base64 -w0 portraits/leotong.png)',
-  hetong: 'data:image/png;base64,'$(base64 -w0 portraits/hetong.png)',
-  hongmao: 'data:image/png;base64,'$(base64 -w0 portraits/hongmao.png')
-};
